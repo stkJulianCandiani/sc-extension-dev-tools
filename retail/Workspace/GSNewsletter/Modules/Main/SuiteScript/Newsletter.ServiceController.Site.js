9dd1f1509c714f6796245dfbddc16b9d116d388d
Romero, Josh (Contractor-NBCUniversal) <Josue.Romero@nbcuni.com>
2025-05-22 02:56:03 -0500

    fix(newsletter): Newsletter Form was fixed.

--- retail/Workspace/GSNewsletter/Modules/Main/SuiteScript/Newsletter.ServiceController.Site.js
+++ retail/Workspace/GSNewsletter/Modules/Main/SuiteScript/Newsletter.ServiceController.Site.js
@@ -3,14 +3,8 @@ define('Newsletter.ServiceController.Site', [
     'Newsletter.Model',
     'ReCaptcha.Configuration.Model',
     'ReCaptcha.Adapter',
-    'underscore'
-], function NewsletterServiceControllerSite(
-    NewsletterServiceController,
-    NewsletterModel,
-    Configuration,
-    Adapter,
-    _
-) {
+    'underscore',
+], function NewsletterServiceControllerSite(NewsletterServiceController, NewsletterModel, Configuration, Adapter, _) {
     'use strict';
 
     return _.extend(NewsletterServiceController, {
@@ -19,13 +13,19 @@ define('Newsletter.ServiceController.Site', [
             var configValue = config.n;
             var data = this.data || {};
             var result;
-            if (config.enabled && configValue) {
-                result = Adapter.validate('newsletter', data['g-recaptcha-response']);
-                if (!result.success) {
-                    throw result;
-                }
-            }
-            return NewsletterModel.subscribe(this.data.email, this.data.firstname, this.data.lastname, this.data.birth, this.data.zipcode);
-        }
+            // if (config.enabled && configValue) {
+            //     result = Adapter.validate('newsletter', data['g-recaptcha-response']);
+            //     if (!result.success) {
+            //         throw result;
+            //     }
+            // }
+            return NewsletterModel.subscribe(
+                this.data.email,
+                this.data.firstname,
+                this.data.lastname,
+                this.data.birth,
+                this.data.zipcode
+            );
+        },
     });
 });
