94aad8c408511e4bbea2d07c5274c8e0afc0c31a
Julian Candiani <julian.candiani@softtek.com>
2025-06-12 19:32:57 -0500

    update(pdp): The Inventory Display has been updated and now it's visible for all users.

--- wholesale/Workspace/RemoveInventoryPerEmail/Modules/Main/Javascript/RemoveInventoryPerEmail.js
+++ wholesale/Workspace/RemoveInventoryPerEmail/Modules/Main/Javascript/RemoveInventoryPerEmail.js
@@ -1,8 +1,5 @@
 /* eslint-disable max-len */
-define('RemoveInventoryPerEmail', [
-    'Profile.Model',
-    'SC.Configuration'
-], function RemoveInventoryPerEmail(
+define('RemoveInventoryPerEmail', ['Profile.Model', 'SC.Configuration'], function RemoveInventoryPerEmail(
     ProfileModel,
     Configuration
 ) {
@@ -12,36 +9,51 @@ define('RemoveInventoryPerEmail', [
         var profile = ProfileModel.getInstance();
         var customerEmail = profile.get('email');
         var emailEnding = Configuration.get('removeinventory.email') || '@girlscouts.org';
-        return customerEmail.indexOf(emailEnding) > -1;
+        return true; //customerEmail.indexOf(emailEnding) > -1;
     }
 
     return {
         mountToApp: function mountToApp(container) {
             var layout = container.getComponent('Layout');
 
-            layout.addToViewContextDefinition('InventoryDisplay.ItemViews.Stock.View', 'isNotAvailableInStore', 'boolean', function isNotAvailableInStoreFn(context) {
-                var isNotAvailableInStore = context.isNotAvailableInStore;
-                if (!isValidEmail()) {
-                    isNotAvailableInStore = false;
+            layout.addToViewContextDefinition(
+                'InventoryDisplay.ItemViews.Stock.View',
+                'isNotAvailableInStore',
+                'boolean',
+                function isNotAvailableInStoreFn(context) {
+                    var isNotAvailableInStore = context.isNotAvailableInStore;
+                    if (!isValidEmail()) {
+                        isNotAvailableInStore = false;
+                    }
+                    return isNotAvailableInStore;
                 }
-                return isNotAvailableInStore;
-            });
+            );
 
-            layout.addToViewContextDefinition('InventoryDisplay.ItemViews.Stock.View', 'showOutOfStockMessage', 'boolean', function showOutOfStockMessageFn(context) {
-                var showOutOfStockMessage = context.showOutOfStockMessage;
-                if (!isValidEmail()) {
-                    showOutOfStockMessage = false;
+            layout.addToViewContextDefinition(
+                'InventoryDisplay.ItemViews.Stock.View',
+                'showOutOfStockMessage',
+                'boolean',
+                function showOutOfStockMessageFn(context) {
+                    var showOutOfStockMessage = context.showOutOfStockMessage;
+                    if (!isValidEmail()) {
+                        showOutOfStockMessage = false;
+                    }
+                    return showOutOfStockMessage;
                 }
-                return showOutOfStockMessage;
-            });
+            );
 
-            layout.addToViewContextDefinition('InventoryDisplay.ItemViews.Stock.View', 'inStockMessageForPDP', 'boolean', function inStockMessageForPDPFn(context) {
-                var inStockMessageForPDP = context.inStockMessageForPDP;
-                if (!isValidEmail()) {
-                    inStockMessageForPDP = false;
+            layout.addToViewContextDefinition(
+                'InventoryDisplay.ItemViews.Stock.View',
+                'inStockMessageForPDP',
+                'boolean',
+                function inStockMessageForPDPFn(context) {
+                    var inStockMessageForPDP = context.inStockMessageForPDP;
+                    if (!isValidEmail()) {
+                        inStockMessageForPDP = false;
+                    }
+                    return inStockMessageForPDP;
                 }
-                return inStockMessageForPDP;
-            });
-        }
+            );
+        },
     };
 });
